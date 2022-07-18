0ff8070d7a147cfa938f4ed2dd3f35b94968e77d
Travis Harry <travisdharry@gmail.com>
2022-07-17 19:16:04 -0500

    test waiverWire

--- app.py
+++ app.py
@@ -286,7 +286,33 @@ def waiverWire():
 
     player_df = get_df("player_df")
 
-    return render_template("waiverWire.html", tables=[player_df.to_html(classes='data')], titles=player_df.columns.values)
+    # Get franchise rosters
+    urlString = f"https://www54.myfantasyleague.com/2022/export?TYPE=rosters&L={user_league}&FRANCHISE={user_franchise}"
+    response = requests.get(urlString)
+    soup = BeautifulSoup(response.content,'xml')
+    data = []
+    franchises = soup.find_all('franchise')
+    for i in range(0,len(franchises)):
+        current_franchise = franchises[i].find_all('player')
+        for j in range(0,len(current_franchise)):
+            rows = [franchises[i].get("id"), franchises[i].get("week"), current_franchise[j].get("id"), current_franchise[j].get("status")]
+            data.append(rows)
+    rosters_df = pd.DataFrame(data)
+
+    # Get Free Agents
+    urlString = f"https://www54.myfantasyleague.com/2022/export?TYPE=freeAgents&L={user_league}"
+    response = requests.get(urlString)
+    soup = BeautifulSoup(response.content,'xml')
+    data = []
+    freeAgents = soup.find_all('player')
+    for i in range(len(freeAgents)):
+        rows = ["FA", "", freeAgents[i].get("id"), "Free Agent"]
+        data.append(rows)
+    fa_df = pd.DataFrame(data)
+    rosters_df = rosters_df.append(fa_df)
+    rosters_df.columns=['FranchiseID','Week','PlayerID','RosterStatus']
+
+    return render_template("waiverWire.html", tables=[rosters_df.to_html(classes='data')], titles=rosters_df.columns.values)
 
 
 
